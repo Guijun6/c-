2cc40cdf1d15ced7673a2ab406db11c4493f8c4e
Guijun6 <1336908711@qq.com>
2017-08-07 10:13:02 +0800

    Add files via upload

---
+++ 07_13_04.c
@@ -0,0 +1,16 @@
+//整形a与b交换
+//#include<stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int y = 0;
+//	printf("a=%d,b=%d\n", a, b);
+//	y = a;
+//	a = b;
+//	b = y;
+//	printf("\na=%d,b=%d\n",a,b);
+//	system("pause");
+//	return 0;
+//}
