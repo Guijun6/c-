88fc785ebfdd6ab282fc7fedd1869d00810353e3
Guijun6 <1336908711@qq.com>
2017-08-07 10:17:10 +0800

    Add files via upload

---
+++ 07_13_05.c
@@ -0,0 +1,19 @@
+//交换变量（不允许创建临时变量）
+//a=5  101
+//b=3  011
+//a     110
+//b     101
+//a     011
+//#include<stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	int a = 5, b = 3;
+//	printf("a=%d,b=%d\n", a, b);
+//	a = a^b;
+//	b = a^b;
+//	a = a^b;
+//	printf("a=%d,b=%d\n", a, b);
+//	system("pause");
+//	return 0;
+//}
