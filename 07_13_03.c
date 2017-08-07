dcc7e092e3990f8b0c85b562b166230d937b67e6
Guijun6 <1336908711@qq.com>
2017-08-07 10:12:44 +0800

    Add files via upload

---
+++ 07_13_03.c
@@ -0,0 +1,29 @@
+//输出1000到2000年之间的闰年
+//#include <stdio.h>
+//#include <stdlib.h>
+//int is_leap_year(int year)
+//{
+//	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+//	{
+//		return 1;
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int year = 0;
+//	int count = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if (is_leap_year(year))
+//		{
+//			printf("%d ", year);
+//			count++;
+//		}
+//
+//	}
+//	printf("\ncount=%d\n", count);
+//	system("pause");
+//	return 0;
+//}
