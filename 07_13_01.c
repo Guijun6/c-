609ed5e13af0afb2e30e3bb41a7d6f222f9e27ee
Guijun6 <1336908711@qq.com>
2017-08-07 09:36:41 +0800

    Add files via upload

---
+++ 07_13_01.c
@@ -0,0 +1,34 @@
+//输出9*9乘法口诀表
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	int i = 0;
+// 	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%d ", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%d ", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
